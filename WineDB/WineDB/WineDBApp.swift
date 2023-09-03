99f3e236aedd77af1f4bc7803e06095e0a4b6086
dhurian <dhurian@gmail.com>
2023-09-03 16:52:36 +0200

    Initial Commit

---
+++ WineDB/WineDB/WineDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  WineDBApp.swift
+//  WineDB
+//
+//  Created by Dhurian Vitoldas on 03/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct WineDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
